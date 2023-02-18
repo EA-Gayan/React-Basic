91049003bd2b94f1497584e49120b4972c8873cd
Gayan <89770956+EA-Gayan@users.noreply.github.com>
2023-02-18 21:48:37 +0530

    Update Btn.js

--- src/Btn.js
+++ src/Btn.js
@@ -1,3 +1,20 @@
+// Handling events using separate function declarations
+// function App() {
+//         function thirdExample() {
+//             console.log('third example');
+//         };
+//         return (
+//             <div className="thirdExample">
+//                 <button onClick={thirdExample}>
+//                     using a separate function declaration
+//                 </button>
+//             </div>
+//         );
+//     };
+
+
+
+
 function Btn(){
     const clickHandler = 
         ()=>console.log('clicked')
